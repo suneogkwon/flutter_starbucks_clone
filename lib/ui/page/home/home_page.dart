8166f9bf8a42bcfce33deb1437742391f29aebe5
kwonsuneog <tnsdjr7@gmail.com>
2022-01-06 18:13:09 +0900

    start make home page

---
+++ lib/ui/page/home/home_page.dart
@@ -0,0 +1,4 @@
+/// Created by IntelliJ IDEA.
+/// User: brighten17
+/// Date: 2022-01-06
+/// Time: 오후 6:10
---
+++ lib/ui/widget/layout/app_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Created by IntelliJ IDEA.
+/// User: brighten17
+/// Date: 2022-01-06
+/// Time: 오후 6:11
+class AppLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
